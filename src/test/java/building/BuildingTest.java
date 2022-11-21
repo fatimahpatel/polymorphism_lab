308ffbd116b6d3c34b3c5897d878b7110e2ca294
Fatimah Patel <fatimahpatel10@gmail.com>
2022-11-21 21:10:27 +0000

    removed building1 object

--- src/test/java/building/BuildingTest.java
+++ src/test/java/building/BuildingTest.java
@@ -4,10 +4,13 @@ import org.junit.jupiter.api.BeforeEach;
 
 public class BuildingTest {
 
-    private Building building1; //creates a new object called building1 using the Building class
+    private Building building;
+
 
     @BeforeEach
-    public void setUp(){ new Building ("test building 1")} //example object to be used for the tests
+
+
+
 
 
 
