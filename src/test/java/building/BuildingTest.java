b8ef26d164a9827548ba322b1e2af8c0fbdcb12d
Fatimah Patel <fatimahpatel10@gmail.com>
2022-11-21 21:00:50 +0000

    set up example object for test

--- src/test/java/building/BuildingTest.java
+++ src/test/java/building/BuildingTest.java
@@ -1,9 +1,13 @@
 package building;
 
+import org.junit.jupiter.api.BeforeEach;
+
 public class BuildingTest {
 
     private Building building1; //creates a new object called building1 using the Building class
 
+    @BeforeEach
+    public void setUp(){ new Building ("test building 1")} //example object to be used for the tests
 
 
 
