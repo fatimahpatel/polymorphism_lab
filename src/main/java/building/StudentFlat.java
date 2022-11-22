08853963a5b0332384fa7a914abdb00d183bed89
Fatimah Patel <fatimahpatel10@gmail.com>
2022-11-22 17:39:32 +0000

    int for numberOfPartiesHosted changed to long.

--- src/main/java/building/StudentFlat.java
+++ src/main/java/building/StudentFlat.java
@@ -2,7 +2,7 @@ package building;
 public class StudentFlat extends Building {
     private long numberOfPartiesHosted;
 
-    public StudentFlat (String dateOfConstruction, int numberOfFloors, int numberOfRooms, int numberOfPartiesHosted){
+    public StudentFlat (String dateOfConstruction, int numberOfFloors, int numberOfRooms, long numberOfPartiesHosted){
         super(dateOfConstruction, numberOfFloors, numberOfRooms);
         this.numberOfPartiesHosted = numberOfPartiesHosted;
     }
