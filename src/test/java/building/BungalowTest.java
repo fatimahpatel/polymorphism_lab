cfd2cae79cdd5b54bb6b5329a09dd95307d2b614
Fatimah Patel <fatimahpatel10@gmail.com>
2022-11-22 17:50:10 +0000

    Created example variable called 'bungalow' to be used for testing.

--- src/test/java/building/BungalowTest.java
+++ src/test/java/building/BungalowTest.java
@@ -3,7 +3,10 @@ package building;
 import org.junit.jupiter.api.BeforeEach;
 
 public class BungalowTest {
+
+    private Bungalow bungalow; //created a new test variable called bungalow
     @BeforeEach
     public void setUp(){
+        bungalow = new Bungalow("27/02/2000", 1, 7, true);
     }
 }
