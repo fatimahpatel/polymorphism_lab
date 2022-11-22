1bd92df9a5bcb6c629915c3625a51fb6a688a89e
Fatimah Patel <fatimahpatel10@gmail.com>
2022-11-22 18:32:00 +0000

    Created getters and setters for name and buildings

--- src/test/java/street/Street.java
+++ src/test/java/street/Street.java
@@ -10,7 +10,26 @@ public class Street {
 
     public Street(String name) {
         this.name = name;
-        this.buildings new ArrayList<>();
+        this.buildings =  new ArrayList<>();
     }
+
+    //getter and setters
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public ArrayList<Building> getBuildings(){
+        return buildings;
+    }
+    public void setBuildings(ArrayList<Building> buildings){
+        this.buildings = buildings
+    }
+
+
+
+
 
 }
