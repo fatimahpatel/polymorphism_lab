64712f39b48847eb736a5bf8d0668efcacba7549
Fatimah Patel <fatimahpatel10@gmail.com>
2022-11-22 17:39:00 +0000

    Office extends from Building. Constructor written using properties from Building, and centralHeating initalised.

--- src/main/java/building/Office.java
+++ src/main/java/building/Office.java
@@ -1,7 +1,11 @@
 package building;
 
-public class Office {
+public class Office extends Building {
 
     private String companyName;
+
+    public Office(String dateOfConstruction, int numberOfFloors, int numberOfRooms, String companyName){
+        super(dateOfConstruction, numberOfFloors, numberOfRooms);
+    }
 
 }
