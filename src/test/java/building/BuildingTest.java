e286d734dc7108f6a5e486d5dca7d6587899e700
Fatimah Patel <fatimahpatel10@gmail.com>
2022-11-22 15:03:39 +0000

    Created constructor for test.

--- src/test/java/building/BuildingTest.java
+++ src/test/java/building/BuildingTest.java
@@ -4,10 +4,18 @@ import org.junit.jupiter.api.BeforeEach;
 
 public class BuildingTest {
 
-    private Building building;
+    private String name;
+    private String dateOfConstruction;
+    private int numberOfFloors;
+    private int numberOfRooms;
 
     @BeforeEach
-    public void setUp(){}
+    public void setUp(String name, String dateOfConstruction, int numberOfFloors, int numberOfRooms){
+        this.name = name;
+        this.dateOfConstruction = dateOfConstruction;
+        this.numberOfFloors = numberOfFloors;
+        this.numberOfRooms = numberOfRooms;
+    }
 
 
 
